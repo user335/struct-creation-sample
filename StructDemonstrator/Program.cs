d7cb7e05f644af073183f98660a45c8d31f7909a
Andy Weekes <userthreethreefive@gmail.com>
2019-02-16 14:04:04 -0700

    first commit

---
+++ StructDemonstrator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemonstrator
+{
+    class Program
+    {
+        public struct Number { public decimal amount { get; set; } };
+        static void Main(string[] args)
+        {
+            Number myno = new Number();
+            myno.amount = 5m;
+            Console.WriteLine("Your precious decimal is " + myno.amount);
+            Console.Read();
+        }
+    }
+}
